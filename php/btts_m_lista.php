4788c8fc1759887d8ba74bb80b260dc2298457f0
Paulo Barros <paulocachulo8@gmail.com>
2023-05-21 16:21:26 +0100

    teste

--- php/btts_m_lista.php
+++ php/btts_m_lista.php
@@ -79,7 +79,9 @@
                     //
                     $consulta_odd="select Odd_BTTS_Yes,Odd_BTTS_No from jogos where Home = (select team_fs from equipas where team_ss = '".$rows['eq_casa']."') and Away = (select team_fs from equipas where team_ss = '".$rows['eq_fora']."')";                    
                     $connection_v2=connection_v2();
+                    
                     $odds=$connection_v2->query($consulta_odd);
+                    echo $odds;
                     $odds=$odds->fetchAll();
                     echo $odds['Odd_BTTS_Yes'];
                     echo $odds['Odd_BTTS_No'];
