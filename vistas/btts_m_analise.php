20b96022f1a773f2f93016177732cadb21ac0bb5
Paulo Barros <paulocachulo8@gmail.com>
2023-07-15 21:10:07 +0100

    pagina de analise

---
+++ vistas/btts_m_analise.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "./php/main.php";
+
+    $id=(isset($_GET['id_up'])) ? $_GET['id_up'] : 0;
+    $id=clean_chain($id);
+?>
+<div class="container is-fluid mb-6">
+        <h1 class="title">Analise</h1>        
+</div>
+
+<div class="container pb-6 pt-6">
+    <?php
+        include "./inc/btn_back.php";
+
+        $check_game=connection();
+        $check_game=$check_game->query("SELECT * FROM prog WHERE id='$id'");
+
+        if ($check_game->rowCount()>0){ 
+            $dados=$check_game->fetch();
+
+    ?>
+</div>
