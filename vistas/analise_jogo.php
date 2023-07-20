a11050406e9c07c20e925de00ee5e0909621a5ba
Paulo Barros <paulocachulo8@gmail.com>
2023-07-20 10:48:33 +0100

    analise prob

--- vistas/analise_jogo.php
+++ vistas/analise_jogo.php
@@ -47,7 +47,7 @@
 
             echo "<div class=\"columns\">";
             echo "<div class=\"column is-1\">";
-            echo "<h3><b>Prob 1</b></h3>";
+            echo "<h3><b>1</b></h3>";
             echo "<div class=\"column is-1\">";
             echo "<h3>".$prob_1."%</h3>";
 
@@ -56,9 +56,45 @@
 
             echo "<div class=\"columns\">";
             echo "<div class=\"column is-1\">";
-            echo "<h3><b>Prob X</b></h3>";
+            echo "<h3><b>X</b></h3>";
             echo "<div class=\"column is-1\">";
             echo "<h3>".$prob_x."%</h3>";
+
+            echo "</div>";
+            echo "</div>";
+
+            echo "<div class=\"columns\">";
+            echo "<div class=\"column is-1\">";
+            echo "<h3><b>2</b></h3>";
+            echo "<div class=\"column is-1\">";
+            echo "<h3>".$prob_2."%</h3>";
+
+            echo "</div>";
+            echo "</div>";
+
+            echo "<div class=\"columns\">";
+            echo "<div class=\"column is-1\">";
+            echo "<h3><b>O2,5</b></h3>";
+            echo "<div class=\"column is-1\">";
+            echo "<h3>".$O25."%</h3>";
+
+            echo "</div>";
+            echo "</div>";
+
+            echo "<div class=\"columns\">";
+            echo "<div class=\"column is-1\">";
+            echo "<h3><b>U2,5</b></h3>";
+            echo "<div class=\"column is-1\">";
+            echo "<h3>".$U25."%</h3>";
+
+            echo "</div>";
+            echo "</div>";
+
+            echo "<div class=\"columns\">";
+            echo "<div class=\"column is-1\">";
+            echo "<h3><b>BTTS</b></h3>";
+            echo "<div class=\"column is-1\">";
+            echo "<h3>".$btts_yes."%</h3>";
 
             echo "</div>";
             echo "</div>";
