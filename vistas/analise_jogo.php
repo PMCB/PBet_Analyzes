a1b79a6e39425ad95e658babbb20ba2b230c0a58
Paulo Barros <paulocachulo8@gmail.com>
2023-07-22 00:08:59 +0100

    Aposta na pag analise

--- vistas/analise_jogo.php
+++ vistas/analise_jogo.php
@@ -10,6 +10,7 @@
 
 <div class="container pb-6 pt-6">
     <?php
+        echo "<a href=\"index.php?vista=bet&id_up='".$id."' class=\"button is-success is-rounded is-small\">Apostar</a>";
         include "./inc/btn_back.php";
 
         $check_conn=connection();
